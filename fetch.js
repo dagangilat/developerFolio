bfd146de24595a6ba7ddbaee787f012f09643546
dagangilat <dagan.gilat@gmail.com>
2023-08-14 13:06:10 +0300

    updates

--- fetch.js
+++ fetch.js
@@ -8,6 +8,8 @@ const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
 const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
 
+print(GITHUB_TOKEN)
+
 const ERR = {
   noUserName:
     "Github Username was found to be undefined. Please set all relevant environment variables.",
